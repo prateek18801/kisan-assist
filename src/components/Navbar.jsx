c98721705efbdf083ad1c43b97dda059dc610c00
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-14 01:34:40 +0530

    Add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <h3 className="logo">Kisan Assist</h3>
+        </div>
+    );
+}
+
+export default Navbar;
