f125eca033c072d21232df2f2ba1e2521fc6c667
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-14 01:34:07 +0530

    Add microphone component

---
+++ src/components/Microphone.jsx
@@ -0,0 +1,13 @@
+import './Microphone.css';
+
+const Microphone = () => {
+    return (
+        <div className='Microphone'>
+            <button>
+                <span className='material-symbols-rounded'>mic</span>
+            </button>
+        </div>
+    );
+}
+
+export default Microphone;
