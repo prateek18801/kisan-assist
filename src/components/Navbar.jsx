0a3bd31e82a474b336fbd43280ae7215efc74d45
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-14 10:53:45 +0530

    Update navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
+import logo from '../assets/logo.png';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div className="Navbar">
-            <h3 className="logo">Kisan Assist</h3>
+        <div className='Navbar'>
+            <div className='Navbar__logo'>
+                <span>Kisan</span>
+                <img src={logo} alt='Kisan Assist' />
+                <span>Assist</span>
+            </div>
         </div>
     );
 }
