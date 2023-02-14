91ca38fb8989b3e378a854bf4681fb5840888cbb
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-14 12:22:08 +0530

    Add status bar

---
+++ src/components/Status.jsx
@@ -0,0 +1,11 @@
+import './Status.css';
+
+const Status = () => {
+    return(
+        <div className="Status">
+            <span>Listening...</span>
+        </div>
+    );
+}
+
+export default Status;
