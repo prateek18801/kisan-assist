299ef565ba1f2460428fd8e6d1178e720c4cf727
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-15 12:09:50 +0530

    Create query status states

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Navbar from '../components/Navbar';
 import Status from '../components/Status';
 import SearchBar from '../components/SearchBar';
@@ -6,12 +8,16 @@ import Microphone from '../components/Microphone';
 import './Home.css';
 
 const Home = () => {
+
+    const [query, setQuery] = useState('');
+    const [status, setStatus] = useState(0);
+
     return (
         <div className='Home'>
             <Navbar />
             <SearchBar />
-            <Status />
-            <Microphone />
+            <Status status={status} />
+            <Microphone status={status} setStatus={setStatus} />
         </div>
     );
 }
