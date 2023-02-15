b21a9c7acd65a4e64ea9eb8a5753dbfeadb52459
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-16 00:01:05 +0530

    Integrate speech recognition states

--- src/components/Microphone.jsx
+++ src/components/Microphone.jsx
@@ -1,11 +1,13 @@
 import './Microphone.css';
 
-const Microphone = ({ status, setStatus }) => {
+const Microphone = ({ status, setStatus, isMicrophoneAvailable, browserSupportsSpeechRecognition }) => {
     return (
         <div className='Microphone'>
             <button
                 style={{ backgroundColor: status === 1 ? 'var(--secondary-color)' : 'var(--primary-color)' }}
                 onClick={() => {
+                    if (!isMicrophoneAvailable) return alert('Microphone permission not granted :(');
+                    if (!browserSupportsSpeechRecognition) return alert('Speech recognition not supported by browser :(');
                     setStatus(current => current === 0 ? 1 : current === 1 ? 2 : 1)
                 }}
             >
