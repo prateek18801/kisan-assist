0534b3eb7e864375fae4fb10152d0f6a85f85fd9
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-15 12:14:40 +0530

    Update styles and status state

--- src/components/Microphone.jsx
+++ src/components/Microphone.jsx
@@ -1,9 +1,14 @@
 import './Microphone.css';
 
-const Microphone = () => {
+const Microphone = ({ status, setStatus }) => {
     return (
         <div className='Microphone'>
-            <button>
+            <button
+                style={{ backgroundColor: status === 1 ? 'var(--secondary-color)' : 'var(--primary-color)' }}
+                onClick={() => {
+                    setStatus(current => current === 0 ? 1 : current === 1 ? 2 : 1)
+                }}
+            >
                 <span className='material-symbols-rounded'>mic</span>
             </button>
         </div>
