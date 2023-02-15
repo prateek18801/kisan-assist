b5aedee7672d64c75d7e7a542767c79db2cf1730
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-16 02:50:06 +0530

    Add search button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,9 @@ const SearchBar = () => {
     return (
         <div className='SearchBar'>
             <input type='text' placeholder='Search' />
-            <span className='material-symbols-rounded'>search</span>
+            <button>
+                <span className='material-symbols-rounded'>search</span>
+            </button>
         </div>
     );
 }
