9a0a85f94e5100fa791288cd3b230f1eeebff3f3
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-20 10:07:46 +0530

    Add cooldown timer

--- src/components/Microphone.jsx
+++ src/components/Microphone.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useRef } from 'react';
+
 import './Microphone.css';
 
-const Microphone = ({ status, setStatus, isMicrophoneAvailable, browserSupportsSpeechRecognition }) => {
+const Microphone = ({ applicationState, setApplicationState, isMicrophoneAvailable, browserSupportsSpeechRecognition }) => {
+
+    const cooldownTimerRef = useRef(null);
+
+    const startCooldownTimer = () => {
+        cooldownTimerRef.current = setTimeout(() => {
+            setApplicationState(2);
+        }, 3000);
+    }
+
+    useEffect(() => {
+        return () => { clearTimeout(cooldownTimerRef.current); }
+    }, []);
+
+
     return (
         <div className='Microphone'>
             <button
-                style={{ backgroundColor: status === 1 ? 'var(--secondary-color)' : 'var(--primary-color)' }}
+                disabled={applicationState === 2}
+                style={{ backgroundColor: applicationState === 1 ? 'var(--secondary-color)' : 'var(--primary-color)' }}
                 onClick={() => {
                     if (!isMicrophoneAvailable) return alert('Microphone permission not granted :(');
                     if (!browserSupportsSpeechRecognition) return alert('Speech recognition not supported by browser :(');
-                    setStatus(current => current === 0 ? 1 : current === 1 ? 2 : 1)
+                    if (applicationState === 0) startCooldownTimer();
+                    setApplicationState(current => current === 1 ? 0 : 1);
                 }}
             >
                 <span className='material-symbols-rounded'>mic</span>
