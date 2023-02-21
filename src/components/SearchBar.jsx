72b5c4f7960cd4a98d22a3c8e6552e73c7ea0184
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-22 01:49:34 +0530

    Implement custom google search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,18 @@
+import { useRef } from 'react';
 import './SearchBar.css';
 
 const SearchBar = () => {
+
+    const searchRef = useRef('');
+
+    const googleSearch = () => {
+        window.location.replace(encodeURI(`https://www.google.com/search?q=${searchRef.current.value.replaceAll(' ', '+')}+site:www.agritech.tnau.ac.in OR site:farmer.gov.in`));
+    }
+
     return (
         <div className='SearchBar'>
-            <input type='text' placeholder='Search' className='notranslate' />
-            <button>
+            <input ref={searchRef} type='text' placeholder='Search' className='notranslate' />
+            <button onClick={googleSearch}>
                 <span className='material-symbols-rounded notranslate'>search</span>
             </button>
         </div>
