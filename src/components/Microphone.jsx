93cd84652914b37e26ae25bf7de0a80242304d86
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-22 01:06:02 +0530

    Remove cooldown timer

--- src/components/Microphone.jsx
+++ src/components/Microphone.jsx
@@ -1,22 +1,6 @@
-import { useEffect, useRef } from 'react';
-
 import './Microphone.css';
 
 const Microphone = ({ applicationState, setApplicationState, isMicrophoneAvailable, browserSupportsSpeechRecognition }) => {
-
-    const cooldownTimerRef = useRef(null);
-
-    const startCooldownTimer = () => {
-        cooldownTimerRef.current = setTimeout(() => {
-            setApplicationState(2);
-        }, 3000);
-    }
-
-    useEffect(() => {
-        return () => { clearTimeout(cooldownTimerRef.current); }
-    }, []);
-
-
     return (
         <div className='Microphone'>
             <button
@@ -25,11 +9,10 @@ const Microphone = ({ applicationState, setApplicationState, isMicrophoneAvailab
                 onClick={() => {
                     if (!isMicrophoneAvailable) return alert('Microphone permission not granted :(');
                     if (!browserSupportsSpeechRecognition) return alert('Speech recognition not supported by browser :(');
-                    if (applicationState === 0) startCooldownTimer();
                     setApplicationState(current => current === 1 ? 0 : 1);
                 }}
             >
-                <span className='material-symbols-rounded'>mic</span>
+                <span className='material-symbols-rounded notranslate'>mic</span>
             </button>
         </div>
     );
