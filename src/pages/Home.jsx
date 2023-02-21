dd43d040b2f8d4537f11e328fd19b7640957c439
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-22 01:13:21 +0530

    Remove useFetch update statehandler update states

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,62 @@
 import { useEffect, useState } from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import useFetch from '../hooks/useFetch';
 
 import Navbar from '../components/Navbar';
 import Status from '../components/Status';
 import QueryBox from '../components/QueryBox';
 import SearchBar from '../components/SearchBar';
 import AnswerBox from '../components/AnswerBox';
+import Translate from '../components/Translate';
 import Microphone from '../components/Microphone';
+import SubmitQuery from '../components/SubmitQuery';
 
 import './Home.css';
 
 const Home = () => {
 
-    const [url, setUrl] = useState('');
+    const [answer, setAnswer] = useState('');
     const [queryText, setQueryText] = useState('');
     const [applicationState, setApplicationState] = useState(0);
 
-    const { response, loading, error, setResponse } = useFetch(url);
-
+    const { speak } = useSpeechSynthesis();
     const { transcript, isMicrophoneAvailable, browserSupportsSpeechRecognition, resetTranscript } = useSpeechRecognition();
+
+    const fetchAnswer = async () => {
+        try {
+            const response = await fetch(encodeURI(`${process.env.REACT_APP_API_BASE_URL}/api/v1/answer?q=${queryText}`));
+            const json = await response.json();
+            if (json.ok) {
+                setAnswer(json.message);
+                setApplicationState(3);
+            }
+            else alert('Invalid response');
+        } catch (err) {
+            alert('Failed to fetch' + err.message);
+        }
+    }
+
+    const speakAnswer = () => { speak({ text: answer }); }
 
     useEffect(() => {
         switch (applicationState) {
             case 0:
-                setUrl('');
-                setResponse('');
-                SpeechRecognition.stopListening();
+                SpeechRecognition.abortListening();
                 break;
             case 1:
                 SpeechRecognition.startListening({ continuous: true });
                 break;
             case 2:
-                setUrl(encodeURI(`${process.env.REACT_APP_API_BASE_URL}/api/v1/answer?q=${queryText}`));
                 break;
             case 3:
+                speak({ text: answer });
                 break;
             default:
                 break;
         }
-    }, [applicationState, queryText, setResponse]);
+    }, [applicationState, speak, answer]);
 
-    useEffect(() => {
-        if (!loading) setApplicationState(current => current === 2 ? 3 : current);
-    }, [loading]);
-
-    useEffect(() => {
-        if (error) alert('Failed to connect to service :(');
-    }, [error]);
-
-    useEffect(() => {
-        setQueryText(transcript);
-    }, [transcript]);
+    useEffect(() => { setQueryText(transcript); }, [transcript]);
 
     return (
         <div className='Home'>
@@ -60,17 +65,25 @@ const Home = () => {
             <Status applicationState={applicationState} />
             <QueryBox
                 queryText={queryText}
+                setAnswer={setAnswer}
                 setQueryText={setQueryText}
                 resetTranscript={resetTranscript}
                 applicationState={applicationState}
                 setApplicationState={setApplicationState}
             />
-            <AnswerBox response={response} />
+            <AnswerBox answer={answer} />
+            <Translate />
             <Microphone
                 applicationState={applicationState}
                 setApplicationState={setApplicationState}
                 isMicrophoneAvailable={isMicrophoneAvailable}
                 browserSupportsSpeechRecognition={browserSupportsSpeechRecognition}
+            />
+            <SubmitQuery
+                fetchAnswer={fetchAnswer}
+                speakAnswer={speakAnswer}
+                applicationState={applicationState}
+                setApplicationState={setApplicationState}
             />
         </div>
     );
